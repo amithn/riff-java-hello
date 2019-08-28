e0017f0371fd278b238909dfee9a0a8c844a4f74
amithn <amith.nmbr@gmail.com>
2019-08-29 00:43:10 +1000

    Initial

--- src/main/java/functions/Hello.java
+++ src/main/java/functions/Hello.java
@@ -5,6 +5,10 @@ import java.util.function.Function;
 public class Hello implements Function<String, String> {
 
 	public String apply(String name) {
+                try {
+		   Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
 		return "Hello " + name;
 	}
 }
